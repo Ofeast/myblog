34d6e637ff31c5f4e3a0d6adf9ecc09f60598de1
window <1005910519@qq.com>
2015-11-04 09:31:45 +0800

    封闭代码

--- myblog/public/javascripts/index.js
+++ myblog/public/javascripts/index.js
@@ -1,97 +1,99 @@
-var header = $('div.header');
-var navList = $('#navList');
-var navPage = $('#navPage');
-var content = $('#content');
-var searchBtn = $('#searchBtn');
-var searchBox = $('#searchBox');
-var pageNum = 1;
+~function(){
+	var header = $('div.header');
+	var navList = $('#navList');
+	var navPage = $('#navPage');
+	var content = $('#content');
+	var searchBtn = $('#searchBtn');
+	var searchBox = $('#searchBox');
+	var pageNum = 1;
 
-var search = window.location.hash.replace('#','');
-// var typeName = search.split('=')[1];
-// alert(search)
-var aLi=navList.find('li');
-var aType=navList.find('a');
-for (var i = 0; i < aType.length; i++) {
-	if (search == aType[i].innerHTML) {
-		aLi.removeClass('active');
-		$(aLi[i]).addClass('active');
-	}
-};
-
-navList.on('click','li',function(){
-	var _this=$(this);
-	navList.find('li').removeClass('active');
-	_this.addClass('active');
-})
-
-
-navPage.on('click','a',function(){
-	var _this=$(this);
-	if(_this.attr('data-pageNum')=='prev'){
-		pageNum--;
-		if(pageNum<1){
-			pageNum=1;
-			return;
+	var search = window.location.hash.replace('#','');
+	// var typeName = search.split('=')[1];
+	// alert(search)
+	var aLi=navList.find('li');
+	var aType=navList.find('a');
+	for (var i = 0; i < aType.length; i++) {
+		if (search == aType[i].innerHTML) {
+			aLi.removeClass('active');
+			$(aLi[i]).addClass('active');
 		}
-	}else if(_this.attr('data-pageNum')=='next'){
-		pageNum++;
-		if(pageNum>navPage.children().length-2){
-			pageNum=navPage.children().length-2
-			return;
-		}
-	}else{
-		pageNum=_this.attr('data-pageNum');
-	}
-	getListByPageCount();
-});
+	};
 
-function getListByPageCount(){
-	$.ajax({
-		url:'/getListByPageCount',
-		data:{
-			pageNum:pageNum
-		},
-		success: function(str){
-			var list = eval('('+str+')');
-			var innerHTML = createList(list);
-			content.html(innerHTML);
+	navList.on('click','li',function(){
+		var _this=$(this);
+		navList.find('li').removeClass('active');
+		_this.addClass('active');
+	})
+
+
+	navPage.on('click','a',function(){
+		var _this=$(this);
+		if(_this.attr('data-pageNum')=='prev'){
+			pageNum--;
+			if(pageNum<1){
+				pageNum=1;
+				return;
+			}
+		}else if(_this.attr('data-pageNum')=='next'){
+			pageNum++;
+			if(pageNum>navPage.children().length-2){
+				pageNum=navPage.children().length-2
+				return;
+			}
+		}else{
+			pageNum=_this.attr('data-pageNum');
+		}
+		getListByPageCount();
+	});
+
+	function getListByPageCount(){
+		$.ajax({
+			url:'/getListByPageCount',
+			data:{
+				pageNum:pageNum
+			},
+			success: function(str){
+				var list = eval('('+str+')');
+				var innerHTML = createList(list);
+				content.html(innerHTML);
+			}
+		});
+	}
+
+	function createList(list){
+		var innerHTML = '';
+		for (var i = 0; i < list.length; i++) {
+			innerHTML+='<a href="/article/'+list[i].id+'" class="list-group-item"> \
+							<label>['+list[i].type+']</label>'+list[i].title+' \
+							<span class="badge">'+list[i].time+'</span> \
+						</a>'
+		};
+		return innerHTML;
+	}
+
+	function createPageCount(pageCount){
+		var str='';
+		for (var i = 1; i <= pageCount; i++) {
+			str+='<li><a href="javascript:;"  data-pageNum="'+i+'">'+i+'</a></li>'
+		};
+		return '<li><a href="javascript:;" data-pageNum="prev">上一页</a></li>'+str+'<li><a href="javascript:;"  data-pageNum="next">下一页</a></li>';
+	}
+
+	addWheel(document,function(down){
+		var scrollTop=document.body.scrollTop || document.documentElement.scrollTop;
+		console.log(scrollTop)
+		if(scrollTop<60){
+			header.show();
+			header.css('position','relative');
+		}else{
+			if(down){
+				header.hide();
+				header.css('position','fixed');
+			}else{
+				header.show();
+				header.css('position','fixed');
+			}
+			
 		}
 	});
-}
-
-function createList(list){
-	var innerHTML = '';
-	for (var i = 0; i < list.length; i++) {
-		innerHTML+='<a href="/article/'+list[i].id+'" class="list-group-item"> \
-						<label>['+list[i].type+']</label>'+list[i].title+' \
-						<span class="badge">'+list[i].time+'</span> \
-					</a>'
-	};
-	return innerHTML;
-}
-
-function createPageCount(pageCount){
-	var str='';
-	for (var i = 1; i <= pageCount; i++) {
-		str+='<li><a href="javascript:;"  data-pageNum="'+i+'">'+i+'</a></li>'
-	};
-	return '<li><a href="javascript:;" data-pageNum="prev">上一页</a></li>'+str+'<li><a href="javascript:;"  data-pageNum="next">下一页</a></li>';
-}
-
-addWheel(document,function(down){
-	var scrollTop=document.body.scrollTop || document.documentElement.scrollTop;
-	console.log(scrollTop)
-	if(scrollTop<60){
-		header.show();
-		header.css('position','relative');
-	}else{
-		if(down){
-			header.hide();
-			header.css('position','fixed');
-		}else{
-			header.show();
-			header.css('position','fixed');
-		}
-		
-	}
-})
+}();
--- myblog/public/javascripts/login.js
+++ myblog/public/javascripts/login.js
@@ -1,37 +1,39 @@
-var username = $('#username');
-var password = $('#password');
-var login = $('#login');
-var reg = $('#reg');
+~function(){
+	var username = $('#username');
+	var password = $('#password');
+	var login = $('#login');
+	var reg = $('#reg');
 
-login.on('click',function(){
-	$.ajax({
-		url: '/login',
-		method: 'post',
-		data: {
-			username: username.val(),
-			password: password.val()
-		},
-		success: function(str){
-			var data=$.parseJSON(str);
-			if(data.err){
-				alert(data.msg);
-			}else{
-				 window.location.href='/admin';
+	login.on('click',function(){
+		$.ajax({
+			url: '/login',
+			method: 'post',
+			data: {
+				username: username.val(),
+				password: password.val()
+			},
+			success: function(str){
+				var data=$.parseJSON(str);
+				if(data.err){
+					alert(data.msg);
+				}else{
+					 window.location.href='/admin';
+				}
 			}
-		}
-	})
-});
+		})
+	});
 
-reg.on('click',function(){
-	$.ajax({
-		url: '/reg',
-		method: 'post',
-		data: {
-			username: username.val(),
-			password: password.val()
-		},
-		success: function(data){
-			alert(data);
-		}
+	reg.on('click',function(){
+		$.ajax({
+			url: '/reg',
+			method: 'post',
+			data: {
+				username: username.val(),
+				password: password.val()
+			},
+			success: function(data){
+				alert(data);
+			}
+		})
 	})
-});
+}();
